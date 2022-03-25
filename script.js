66dba0ea59ab8b8a3cf21089ebcfa94ab1e2c2d0
ProteinK <konprotein@gmail.com>
2022-03-25 14:17:13 +0100

    Connect buttons to display

--- script.js
+++ script.js
@@ -14,6 +14,42 @@ const divide = (a, b) => {
   return a / b;
 }
 
+const operate = (a, operator, b) => {
+  if (operator === '+') {
+    return add(a, b);
+  }
+  if (operator === '-') {
+    return subtract(a, b);
+  }
+  if (operator === '*') {
+    return multiply(a, b);
+  }
+  if (operator === '/') {
+    return divide(a, b);
+  }
+}
+
+const display = document.querySelector('.display');
+
+function clear(e) {
+  display.textContent = 0;
+}
+
+const addToDisplay = token => {
+  if (display.textContent.startsWith('0')) {
+    display.textContent = token;
+  } else {
+    display.textContent += token;
+  }
+};
+
+function buttonClicked(e) {
+  addToDisplay(this.textContent);
+}
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clear);
+
 const buttons = document.querySelector('.buttons');
 
 // Add buttons
@@ -21,6 +57,7 @@ for (let i = 1; i <= 9; i++) {
   let btn = document.createElement('button');
   btn.id = `button${i}`;
   btn.textContent = i;
+  btn.addEventListener('click', buttonClicked);
   buttons.appendChild(btn);
 }
 
@@ -28,4 +65,5 @@ for (let i = 1; i <= 9; i++) {
 let btn = document.createElement('button');
 btn.id = 'button0';
 btn.textContent = 0;
+btn.addEventListener('click', buttonClicked);
 buttons.appendChild(btn);
